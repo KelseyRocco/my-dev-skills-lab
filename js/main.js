4dbe3b7015cc045f7f2f13834f765376c8420d4d
KelseyRocco <kelseyroccoh@gmail.com>
2021-03-27 14:17:58 -0400

    "added display none to ul in jquery"

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+    //first want to delete everything in <seciton.list>
+    
+    $("ul").css("display", "none");
+
+    //make <button#addSkill> functional
+    //--->when clicked, add whatever is in text input area come up in <section.list>
+
+    var newSkills =  ["Cascading Style Sheets (CSS)", "Hypertext Markup Language (HTML)"]   
+
+
+
+
+    $("#addSkill").on("click", function(){
+        let 
+
+    })
+
+
+
+    //make <button.delete> functional
+    //---> when clicked, make disappear from <section.list>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
