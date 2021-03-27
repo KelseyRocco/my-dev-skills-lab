29872bfa9942182ba515f5bc3de76584450efad3
KelseyRocco <kelseyroccoh@gmail.com>
2021-03-27 17:37:33 -0400

    finished jQuery lab

--- js/main.js
+++ js/main.js
@@ -1,41 +1,31 @@
 $(document).ready(function(){
 
-    //first want to delete everything in <seciton.list>
+    //As a User (AAU), I don't want to see any developer skills when the page first loads so that I can start with a fresh slate.
     
     $("ul").css("display", "none");
 
-    //make <button#addSkill> functional
-    //--->when clicked, add whatever is in text input area come up in <section.list>
+    //AAU, I want to be able to type in a skill and have it added to my list of skills by clicking a button.
+    //I know the code below doesn't work, but I figured I would play out how I was hoping it should work and then see exactly how far off I am!
 
-    var newSkills =  ["Cascading Style Sheets (CSS)", "Hypertext Markup Language (HTML)"]   
+    function newLi(){
+        var skill = $(`<li class="newSkill"></li>`);
 
+        $("ul").append(skill);
+        $("newSkill").append(input=text);
+    };
 
-
-
-    $("#addSkill").on("click", function(){
-        let 
-
-    })
-
-
-
-    //make <button.delete> functional
-    //---> when clicked, make disappear from <section.list>
+        $("#addSkill").on("click", function(){
+            $("ul").append(skill);
+        
+    });
 
 
+    //AAU, I want to be able to remove an individual skill one at a time in case I make a mistake.
+
+    $("#delete").on("click", function(){
+        $("#delete li").remove
+
+    });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
+});
